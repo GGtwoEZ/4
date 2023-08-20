edec1d9c4b680e6fee668b0094fac7441c2b9f6b
GGtwoEZ <dkanehiro@gmail.com>
2023-08-20 19:10:43 +0700

    new attempt baby

---
+++ Pull simulator.js
@@ -0,0 +1,201 @@
+const summonButton = document.getElementById('summon-button');
+const resultDiv = document.getElementById('result');
+const clickCountSpan = document.getElementById('clickCount');
+const displayOneResultButton = document.getElementById('display-one-result'); // New button
+
+const characters = [
+    { rarity: '3-star', name: '3 star lightcone', odds: 94.3, avatar: '../kys.gif'},
+    { rarity: '4-star', name: 'Arlan', odds: 0.17, avatar: '../arlan.png' },
+    { rarity: '4-star', name: 'Asta', odds: 0.17, avatar: '../asta.png' },
+    { rarity: '4-star', name: 'Dan Heng', odds: 0.17, avatar: '../danheng.png' },
+    { rarity: '4-star', name: 'Herta', odds: 0.17, avatar: '../cunny.png' },
+    { rarity: '4-star', name: 'Hook', odds: 0.17, avatar: '../cunny.png' },
+    { rarity: '4-star', name: 'March 7th', odds: 0.17, avatar: '../march 7th.png' },
+    { rarity: '4-star', name: 'Natasha', odds: 0.17, avatar: '../MOMMY.png' },
+    { rarity: '4-star', name: 'Pela', odds: 0.17, avatar: '../cunny.png' },
+    { rarity: '4-star', name: 'Qingque', odds: 0.17, avatar: '../cunny.png' },
+    { rarity: '4-star', name: 'Sampo', odds: 0.17, avatar: '../sampo.png' },
+    { rarity: '4-star', name: 'Serval', odds: 0.17, avatar: '../serval.png' },
+    { rarity: '4-star', name: 'Sushang', odds: 0.17, avatar: '../sus.gif' },
+    { rarity: '4-star', name: 'Tingyun', odds: 0.17, avatar: '../Tingyun.png' },
+    { rarity: '4-star', name: 'Yukong', odds: 0.17, avatar: '../yukong.png' },
+    { rarity: '4-star', name: 'Luka', odds: 0.17, avatar: '../luka.png' },
+    { rarity: '5-star', name: 'Bailu', odds: 0.04285714286, avatar: '../cunny.png' },
+    { rarity: '5-star', name: 'Bronya', odds: 0.04285714286, avatar: '../bronya.png' },
+    { rarity: '5-star', name: 'Clara', odds: 0.04285714286, avatar: '../cunny.png' },
+    { rarity: '5-star', name: 'Gepard', odds: 0.04285714286, avatar: '../gepard.png' },
+    { rarity: '5-star', name: 'Himeko', odds: 0.04285714286, avatar: '../himeko.png' },
+    { rarity: '5-star', name: 'Welt', odds: 0.04285714286, avatar: '../welt.png' },
+    { rarity: '5-star', name: 'Yanqing', odds: 0.04285714286, avatar: '../yangqing.png' },
+    { rarity: '5-star', name: 'But the Battle Is not Over', odds: 0.04285714286, avatar: '../safe.gif' },
+    { rarity: '5-star', name: 'In The Name of the World', odds: 0.04285714286, avatar: '../safe.gif' },
+    { rarity: '5-star', name: 'Moment of Victory', odds: 0.04285714286, avatar: '../safe.gif' },
+    { rarity: '5-star', name: 'Night on the Milky Way', odds: 0.04285714286, avatar: '../safe.gif' },
+    { rarity: '5-star', name: 'Something Irreplaceable', odds: 0.04285714286, avatar: '../safe.gif' },
+    { rarity: '5-star', name: 'Sleep Like the Dead', odds: 0.04285714286, avatar: '../safe.gif' },
+    { rarity: '5-star', name: 'Time Waits for No One', odds: 0.04285714286, avatar: '../safe.gif' },
+    { rarity: '4-star', name: '4 Star lightcone', odds:2.55, avatar: '../skull.jpg'},
+];
+
+// Initialize click count
+let clickCount = 0;
+
+summonButton.addEventListener('click', () => {
+    resultDiv.innerHTML = ''; // Clear previous results
+
+    
+    for (let i = 0; i < 9; i++) {
+        
+        const randomNumber = Math.random() * 100;
+        let accumulatedOdds = 0;
+
+        for (const character of characters) {
+            accumulatedOdds += character.odds;
+
+            if (randomNumber <= accumulatedOdds) {
+                resultDiv.innerHTML += `
+                <p>You summoned ${character.name} </p>
+                <img src=" ${character.avatar}">
+                `;
+                
+                break;
+            }
+        }
+    }
+    // Increment the click count by 10
+    clickCount += 10;
+
+    // Update the click count displayed on the page
+    clickCountSpan.textContent = clickCount;
+
+    // You can add additional logic here for character summoning if needed
+  // Check if the clickCount is a multiple of 10 and summon a 4star character if available
+  if (clickCount >= 10 && clickCount % 10 === 0 && clickCount % 90 !== 0) {
+    const fourStarCharacters = characters.filter(character => character.rarity === '4star');
+    const randomIndex = Math.floor(Math.random() * fourStarCharacters.length);
+    const character = fourStarCharacters[randomIndex];
+
+    if (character) {
+        resultDiv.innerHTML += `
+        <p>You summoned ${character.name} </p>
+        <img src="${character.avatar}">
+        `;
+        }
+     }
+
+     // Check if the clickCount is a multiple of 90 and summon a 5star character if available
+  if (clickCount >= 90 && clickCount % 90 === 0) {
+    const fiveStarCharacters = characters.filter(character => character.rarity === '5star');
+    const randomIndex = Math.floor(Math.random() * fiveStarCharacters.length);
+    const character = fiveStarCharacters[randomIndex];
+
+    if (character) {
+        resultDiv.innerHTML += `
+        <p>You summoned ${character.name} </p>
+        <img src="${character.avatar}">
+        `;
+        }
+     }
+});
+
+// Add a click event listener to the "Display One Result" button
+displayOneResultButton.addEventListener('click', () => {
+    // Clear previous results
+    resultDiv.innerHTML = '';
+
+    // Simulate summoning one character
+    const randomNumber = Math.random() * 100;
+    let accumulatedOdds = 0;
+
+    for (const character of characters) {
+        accumulatedOdds += character.odds;
+
+        if (randomNumber <= accumulatedOdds) {
+            resultDiv.innerHTML = `
+            <p>You summoned ${character.name} </p>
+            <img src="${character.avatar}">
+            `;
+            break;
+        }
+    }
+    // Increment the click count by 1
+    clickCount ++;
+
+    // Update the click count displayed on the page
+    clickCountSpan.textContent = clickCount;
+
+  // Check if the clickCount is a multiple of 10 and summon a 4star character if available
+  if (clickCount % 10 === 0 && clickCount % 90 !== 0) {
+        // Clear previous results
+        resultDiv.innerHTML = '';
+    const fourStarCharacters = characters.filter(character => character.rarity === '4star');
+    const randomIndex = Math.floor(Math.random() * fourStarCharacters.length);
+    const character = fourStarCharacters[randomIndex];
+
+    if (character) {
+        resultDiv.innerHTML += `
+        <p>You summoned ${character.name} </p>
+        <img src="${character.avatar}">
+        `;
+        }
+     }
+
+     // Check if the clickCount is a multiple of 90 and summon a 5star character if available
+  if (clickCount % 90 === 0) {
+        // Clear previous results
+        resultDiv.innerHTML = '';
+    const fiveStarCharacters = characters.filter(character => character.rarity === '5star');
+    const randomIndex = Math.floor(Math.random() * fiveStarCharacters.length);
+    const character = fiveStarCharacters[randomIndex];
+
+    if (character) {
+        resultDiv.innerHTML += `
+        <p>You summoned ${character.name} </p>
+        <img src="${character.avatar}">
+        `;
+        }
+     }
+});
+
+const audio = document.getElementById('audio');
+const playStopButton = document.getElementById('playStopButton');
+
+playStopButton.addEventListener('click', () => {
+  if (audio.paused) {
+    audio.play();
+    playStopButton.style.backgroundImage = "url('../PlayButton.png')";
+  } else {
+    audio.pause();
+    audio.currentTime = 0;
+    playStopButton.style.backgroundImage = "url('../PauseButton.png')";
+  }
+});
+
+// Sfx for button 
+const hoverSound = document.getElementById("hoverSound");
+const clickSound = document.getElementById("clickSound");
+
+displayOneResultButton.addEventListener("mouseover", () => {
+    hoverSound.currentTime = 0; // Rewind sound to the beginning
+    hoverSound.play();
+});
+
+displayOneResultButton.addEventListener("click", () => {
+    clickSound.currentTime = 0; // Rewind sound to the beginning
+    clickSound.play();
+});
+
+summonButton.addEventListener("mouseover", () => {
+    hoverSound.currentTime = 0; // Rewind sound to the beginning
+    hoverSound.play();
+});
+
+summonButton.addEventListener("click", () => {
+    clickSound.currentTime = 0; // Rewind sound to the beginning
+    clickSound.play();
+});
+
+playStopButton.addEventListener('click', () => {
+    hoverSound.currentTime = 0; // Rewind sound to the beginning
+    hoverSound.play();
+});
