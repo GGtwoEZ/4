ab9a90a43ff6a0cbe3c5462db30f464b6a6a23c3
GGtwoEZ <141321051+GGtwoEZ@users.noreply.github.com>
2023-08-20 19:56:35 +0700

    Update Pull simulator.js

--- Pull simulator.js
+++ Pull simulator.js
@@ -4,37 +4,37 @@ const clickCountSpan = document.getElementById('clickCount');
 const displayOneResultButton = document.getElementById('display-one-result'); // New button
 
 const characters = [
-    { rarity: '3star', name: '3star lightcone', odds: 94.3, avatar: '../4/kys.gif'},
-    { rarity: '4star', name: 'Arlan', odds: 0.17, avatar: '../4/arlan.png' },
-    { rarity: '4star', name: 'Asta', odds: 0.17, avatar: '../4/asta.png' },
-    { rarity: '4star', name: 'Dan Heng', odds: 0.17, avatar: '../4/danheng.png' },
-    { rarity: '4star', name: 'Herta', odds: 0.17, avatar: '../4/cunny.png' },
-    { rarity: '4star', name: 'Hook', odds: 0.17, avatar: '../4/cunny.png' },
-    { rarity: '4star', name: 'March 7th', odds: 0.17, avatar: '../4/march 7th.png' },
-    { rarity: '4star', name: 'Natasha', odds: 0.17, avatar: '../4/MOMMY.png' },
-    { rarity: '4star', name: 'Pela', odds: 0.17, avatar: '../4/cunny.png' },
-    { rarity: '4star', name: 'Qingque', odds: 0.17, avatar: '../4/cunny.png' },
-    { rarity: '4star', name: 'Sampo', odds: 0.17, avatar: '../4/sampo.png' },
-    { rarity: '4star', name: 'Serval', odds: 0.17, avatar: '../4/serval.png' },
-    { rarity: '4star', name: 'Sushang', odds: 0.17, avatar: '../4/sus.gif' },
-    { rarity: '4star', name: 'Tingyun', odds: 0.17, avatar: '../4/Tingyun.png' },
-    { rarity: '4star', name: 'Yukong', odds: 0.17, avatar: '../4/yukong.png' },
-    { rarity: '4star', name: 'Luka', odds: 0.17, avatar: '../4/luka.png' },
-    { rarity: '5star', name: 'Bailu', odds: 0.04285714286, avatar: '../4/cunny.png' },
-    { rarity: '5star', name: 'Bronya', odds: 0.04285714286, avatar: '../4/bronya.png' },
-    { rarity: '5star', name: 'Clara', odds: 0.04285714286, avatar: '../4/cunny.png' },
-    { rarity: '5star', name: 'Gepard', odds: 0.04285714286, avatar: '../4/gepard.png' },
-    { rarity: '5star', name: 'Himeko', odds: 0.04285714286, avatar: '../4/himeko.png' },
-    { rarity: '5star', name: 'Welt', odds: 0.04285714286, avatar: '../4/welt.png' },
-    { rarity: '5star', name: 'Yanqing', odds: 0.04285714286, avatar: '../4/yangqing.png' },
-    { rarity: '5star', name: 'But the Battle Is not Over', odds: 0.04285714286, avatar: '../4/safe.gif' },
-    { rarity: '5star', name: 'In The Name of the World', odds: 0.04285714286, avatar: '../4/safe.gif' },
-    { rarity: '5star', name: 'Moment of Victory', odds: 0.04285714286, avatar: '../4/safe.gif' },
-    { rarity: '5star', name: 'Night on the Milky Way', odds: 0.04285714286, avatar: '../4/safe.gif' },
-    { rarity: '5star', name: 'Something Irreplaceable', odds: 0.04285714286, avatar: '../4/safe.gif' },
-    { rarity: '5star', name: 'Sleep Like the Dead', odds: 0.04285714286, avatar: '../4/safe.gif' },
-    { rarity: '5star', name: 'Time Waits for No One', odds: 0.04285714286, avatar: '../4/safe.gif' },
-    { rarity: '4star', name: '4 Star lightcone', odds:2.55, avatar: '../4/skull.jpg'},
+    { rarity: '3-star', name: '3 star lightcone', odds: 94.3, avatar: '../4/kys.gif'},
+    { rarity: '4-star', name: 'Arlan', odds: 0.17, avatar: '../4/arlan.png' },
+    { rarity: '4-star', name: 'Asta', odds: 0.17, avatar: '../4/asta.png' },
+    { rarity: '4-star', name: 'Dan Heng', odds: 0.17, avatar: '../4/danheng.png' },
+    { rarity: '4-star', name: 'Herta', odds: 0.17, avatar: '../4/cunny.png' },
+    { rarity: '4-star', name: 'Hook', odds: 0.17, avatar: '../4/cunny.png' },
+    { rarity: '4-star', name: 'March 7th', odds: 0.17, avatar: '../4/march 7th.png' },
+    { rarity: '4-star', name: 'Natasha', odds: 0.17, avatar: '../4/MOMMY.png' },
+    { rarity: '4-star', name: 'Pela', odds: 0.17, avatar: '../4/cunny.png' },
+    { rarity: '4-star', name: 'Qingque', odds: 0.17, avatar: '../4/cunny.png' },
+    { rarity: '4-star', name: 'Sampo', odds: 0.17, avatar: '../4/sampo.png' },
+    { rarity: '4-star', name: 'Serval', odds: 0.17, avatar: '../4/serval.png' },
+    { rarity: '4-star', name: 'Sushang', odds: 0.17, avatar: '../4/sus.gif' },
+    { rarity: '4-star', name: 'Tingyun', odds: 0.17, avatar: '../4/Tingyun.png' },
+    { rarity: '4-star', name: 'Yukong', odds: 0.17, avatar: '../4/yukong.png' },
+    { rarity: '4-star', name: 'Luka', odds: 0.17, avatar: '../4/luka.png' },
+    { rarity: '5-star', name: 'Bailu', odds: 0.04285714286, avatar: '../4/cunny.png' },
+    { rarity: '5-star', name: 'Bronya', odds: 0.04285714286, avatar: '../4/bronya.png' },
+    { rarity: '5-star', name: 'Clara', odds: 0.04285714286, avatar: '../4/cunny.png' },
+    { rarity: '5-star', name: 'Gepard', odds: 0.04285714286, avatar: '../4/gepard.png' },
+    { rarity: '5-star', name: 'Himeko', odds: 0.04285714286, avatar: '../4/himeko.png' },
+    { rarity: '5-star', name: 'Welt', odds: 0.04285714286, avatar: '../4/welt.png' },
+    { rarity: '5-star', name: 'Yanqing', odds: 0.04285714286, avatar: '../4/yangqing.png' },
+    { rarity: '5-star', name: 'But the Battle Is not Over', odds: 0.04285714286, avatar: '../4/safe.gif' },
+    { rarity: '5-star', name: 'In The Name of the World', odds: 0.04285714286, avatar: '../4/safe.gif' },
+    { rarity: '5-star', name: 'Moment of Victory', odds: 0.04285714286, avatar: '../4/safe.gif' },
+    { rarity: '5-star', name: 'Night on the Milky Way', odds: 0.04285714286, avatar: '../4/safe.gif' },
+    { rarity: '5-star', name: 'Something Irreplaceable', odds: 0.04285714286, avatar: '../4/safe.gif' },
+    { rarity: '5-star', name: 'Sleep Like the Dead', odds: 0.04285714286, avatar: '../4/safe.gif' },
+    { rarity: '5-star', name: 'Time Waits for No One', odds: 0.04285714286, avatar: '../4/safe.gif' },
+    { rarity: '4-star', name: '4 Star lightcone', odds:2.55, avatar: '../4/skull.jpg'},
 ];
 
 // Initialize click count
@@ -43,7 +43,7 @@ let clickCount = 0;
 summonButton.addEventListener('click', () => {
     resultDiv.innerHTML = ''; // Clear previous results
 
-    
+    //Can't believe all I spent 2 hour to make the result only 10 not 11, all I have to do is to make 'i < 10' to 'i < 9'
     for (let i = 0; i < 9; i++) {
         
         const randomNumber = Math.random() * 100;
@@ -62,9 +62,6 @@ summonButton.addEventListener('click', () => {
             }
         }
     }
-            }
-        }
-    }
     // Increment the click count by 10
     clickCount += 10;
 
@@ -74,7 +71,7 @@ summonButton.addEventListener('click', () => {
     // You can add additional logic here for character summoning if needed
   // Check if the clickCount is a multiple of 10 and summon a 4star character if available
   if (clickCount >= 10 && clickCount % 10 === 0 && clickCount % 90 !== 0) {
-    const fourStarCharacters = characters.filter(character => character.rarity === '4star');
+    const fourStarCharacters = characters.filter(character => character.rarity === '4-star');
     const randomIndex = Math.floor(Math.random() * fourStarCharacters.length);
     const character = fourStarCharacters[randomIndex];
 
@@ -88,7 +85,7 @@ summonButton.addEventListener('click', () => {
 
      // Check if the clickCount is a multiple of 90 and summon a 5star character if available
   if (clickCount >= 90 && clickCount % 90 === 0) {
-    const fiveStarCharacters = characters.filter(character => character.rarity === '5star');
+    const fiveStarCharacters = characters.filter(character => character.rarity === '5-star');
     const randomIndex = Math.floor(Math.random() * fiveStarCharacters.length);
     const character = fiveStarCharacters[randomIndex];
 
